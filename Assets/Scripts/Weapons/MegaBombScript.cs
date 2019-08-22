274008147de10eaae05d7f30ef203cbac98748a4
lequocgiom <letrung95@gmail.com>
2019-08-22 19:29:33 +0700

    Mega Bomb

---
+++ Assets/Scripts/Weapons/MegaBombScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class MegaBombScript : MonoBehaviour
+{
+    public float radius = 2f;
+    public float damage = 5f;
+    public ParticleSystem mbFX;
+
+    // Use this for initialization
+    void Start()
+    {
+        var partMain = mbFX.main;
+        partMain.startSize = radius * partMain.startSize.constant;
+
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            DeployBomb();
+        }
+    }
+
+    public void DeployBomb()
+    {
+        print("Bomb Deployed!");
+
+        mbFX.Play();
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            HealthSystem healthSystem = colliders[i].GetComponent<HealthSystem>();
+
+            if (healthSystem != null && colliders[i].CompareTag("Enemy"))
+            {
+                healthSystem.TakeDamage(damage);
+
+                print(colliders[i].name + " is being hit by the bomb");
+            }
+        }
+    }
+}
