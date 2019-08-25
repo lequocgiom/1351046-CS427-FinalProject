38eb67d1ef7bbb055f61cd72c1072b3f7ef535d1
lequocgiom <letrung95@gmail.com>
2019-08-25 23:01:34 +0700

    Touch control

--- Assets/Scripts/Transform/PlayerMovement.cs
+++ Assets/Scripts/Transform/PlayerMovement.cs
@@ -2,39 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
-
+public class PlayerMovement : MonoBehaviour
+{
     public float speed = 5f, bankingValue = 90f;
     public Transform visualChild;
     private Camera cam;
     private Rigidbody myRb;
     private float distance;
     private Vector3 velocity, lastPosition, rotation, touchPos, screenToWorld;
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start()
+    {
         cam = Camera.main;
         myRb = GetComponent<Rigidbody>();
 
         distance = (cam.transform.position - transform.position).y;
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
         velocity = transform.position - lastPosition;
 
         Move();
 
         lastPosition = transform.position;
-	}
+    }
 
     void Move()
     {
+#if UNITY_EDITOR
         touchPos = Input.mousePosition;
+#elif UNITY_ANDROID
+        touchPos = Input.touchCount > 0 ? Input.GetTouch(0).position : (Vector2)touchPos;
+#endif
         touchPos.z = distance;
 
         screenToWorld = cam.ScreenToWorldPoint(touchPos);
 
-        Vector3 movement = Vector3.Lerp(transform.position, screenToWorld, speed * Time.fixedDeltaTime);
+        Vector3 movement = Vector3.Lerp(myRb.position, screenToWorld, speed * Time.fixedDeltaTime);
 
         myRb.MovePosition(movement);
 
